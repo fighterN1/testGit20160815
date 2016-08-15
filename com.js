d633fa5c0a7a431eccce85e7960f83545a95dab3
fighterN1 <l1095780431@163.com>
2016-08-15 17:30:16 +0800

    提交测试

--- com.js
+++ com.js
@@ -1,6 +1,8 @@
 //如果不存在(ie7,8,9)console
 //if(!window.console){window.console=function(){};console.log=function(){}						}
-function GitAdd(){aaaaa};
+function GitAdd(){
+	var a="asfasf";
+	};
 function initConsole(){
 	window.console=function(){}
     console.log=function(){}
